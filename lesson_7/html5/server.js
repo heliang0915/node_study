040034407bbf046cfe97608967b361a8ae3310de
heliang <598481337@qq.com>
2016-03-30 19:18:55 +0800

    提交

--- lesson_7/html5/server.js
+++ lesson_7/html5/server.js
@@ -7,18 +7,33 @@ var util = require("util");
 var querystring = require("querystring");
 var url = require("url");
 var fs = require("fs");
-var formidable=require("formidable")
+var formidable = require("formidable")
 
 var app = http.createServer(function (req, res) {
     var urlOjb = url.parse(req.url, true);
     var pathname = urlOjb.pathname;
     if (pathname == "/") {
-        fs.createReadStream("index.html").pipe(res);
+        fs.createReadStream("./index.html").pipe(res);
     } else if (pathname == "/post") {
-        var parser=new formidable.IncomingForm();
-        parser.parse(req,function(err,fields,files){
-            console.log(files);
+        var parser = new formidable.IncomingForm();
+
+        parser.parse(req, function (err, fields, files) {
+            var fileName = fields.name;
+            var index = fields.index;
+            var total = fields.total;
+            var data = fields.data;
+            var size = fields.size;
+
+            console.log(files.data.path);
+            //var src = fs.createReadStream(files.data.path);
+            //
+            //var target = fs.createWriteStream(fileName + "." + index);
+            //src.pipe(target);
+
+
             res.end("hellos");
+
+
         })
     }
 });
